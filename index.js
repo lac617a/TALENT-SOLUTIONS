4167821f829c3112e89d895151f70a5cb611e43b
lac617a <botlacrita617@gmail.com>
2022-12-02 17:45:56 -0500

    desktop ready

--- index.js
+++ index.js
@@ -1,3 +1,91 @@
-document.addEventListener('DOMContentLoaded', () => {
+const handlePlainTable = (result) => {
+  const main = document.querySelector('main');
+  const form = document.querySelector('.ts-article-form')
+  const fragment = document.createDocumentFragment();
+  const template = document.querySelector('template').content;
 
+  result.forEach((item) => {
+    const { id, title, content, button: b, img: image } = item;
+    const p = document.createElement('p');
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+    const button = document.createElement('button');
+    const ts_section = template.querySelectorAll('.ts-section');
+    ts_section.forEach(node => node.innerHTML = '');
+    
+    h2.textContent = title;
+    p.innerHTML = content;
+    button.textContent = b;
+    img.src = image;
+    img.alt = `${id}-${title}`;
+    
+    if (id % 2 === 0) {
+      img.setAttribute('dir', 'ltr');
+      img.classList.add('is-right');
+      ts_section.item(0).appendChild(img);
+      ts_section.item(1).append(h2, p, button);
+    } else {
+      img.setAttribute('dir', 'rtl');
+      img.classList.add('is-left');
+      ts_section.item(0).append(h2, p, button);
+      ts_section.item(1).appendChild(img);
+    }
+    
+    const clone = template.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  main.append(fragment, form);
+}
+
+
+const optionRoot = {
+  root: document,
+  rootMargin: '0px',
+  threshold: 0
+}
+
+
+const useElementOnScreen = (node, style, option = optionRoot) => {
+  const callback = entries => {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      node.classList.add(style);
+    } else {
+      node.classList.remove(style);
+    }
+  }
+
+  const observer = new IntersectionObserver(callback, option)
+  if (node) {
+    observer.observe(node);
+  } else {
+    observer.unobserve(node);
+  }
+}
+
+const animated = () => {
+  const btn = document.querySelectorAll('.ts-section button');
+  const ts_section = document.querySelectorAll('.ts-section img');
+
+  ts_section.forEach(node => {
+    if (node.getAttribute('dir') == 'ltr') {
+      useElementOnScreen(node, 'is-animate-left');
+    } else {
+      useElementOnScreen(node, 'is-animate-right');
+    }
+
+  });
+
+  btn.forEach(node => {
+    useElementOnScreen(node, 'is-animate');
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('content.json')
+    .then(data =>
+      data.json().then(result => handlePlainTable(result))
+    )
+    .then(animated)
+    .catch(e => console.error(e));
 });
